57ab77ea2e5319ec66ae8bfd27a5c7f3f305f961
TJ Santos <tjscode@outlook.com>
2025-05-25 13:05:02 -0700

    prettier tailwind

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,10 +8,10 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white">
         <img
           src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
+          className="pointer-events-none h-[40vmin] animate-[spin_20s_linear_infinite]"
           alt="logo"
         />
         <p>
