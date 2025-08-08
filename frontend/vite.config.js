25ec85cc12a92e6e971db28c1ae7697c1fc8ddac
TJ Santos <tjscode@outlook.com>
2025-08-07 18:43:19 -0700

    update tanstack router vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,17 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     viteReact(),
     tailwindcss(),
   ],
