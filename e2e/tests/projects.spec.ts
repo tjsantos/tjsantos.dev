8c6e39d07936d15be15b5960c996784e7a1e624e
TJ Santos <tjscode@outlook.com>
2025-09-05 01:41:48 -0700

    fix tests

--- e2e/tests/projects.spec.ts
+++ e2e/tests/projects.spec.ts
@@ -1,26 +1,23 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto('/');
+test('has title', async ({page}) => {
+  await page.goto('/');
 
-    await expect(page).toHaveTitle(/TJ/);
+  await expect(page).toHaveTitle(/TJ/);
 });
 
 const projects = [
-    'testimonial-card',
-    'blog-card',
-    'profile-card'
+  'testimonial-card',
+  'blog-card',
+  'profile-card'
 ]
 
 test('main page should link to all projects', async ({page}) => {
-    await page.goto('/')
+  await page.goto('/')
 
-    // TODO prettier, eslint, tsconfig
-    const links =
-        await page.locator('a')
-            .evaluateAll<Array<string>, HTMLAnchorElement>((selected) => selected.map((a) => a.href))
-
-    projects.forEach((projectName) => {
-        expect(links.some((link) => link.endsWith(projectName))).toBe(true)
-    })
+  // TODO prettier, eslint, tsconfig
+  const assertions = projects.map((projectName) => {
+    return expect(page.locator(`a[href*="${projectName}"]`)).toBeVisible()
+  })
+  await Promise.all(assertions)
 })
