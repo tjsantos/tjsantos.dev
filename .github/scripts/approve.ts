4c6041622d9e1e23d28ce6fbacb0df40b303c582
TJ Santos <tjscode@outlook.com>
2025-10-01 23:37:50 -0700

    octokit logging

--- .github/scripts/approve.ts
+++ .github/scripts/approve.ts
@@ -1,11 +1,11 @@
 import { Octokit } from 'octokit'
 
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN, log: console })
 
-console.log('Hello, World!')
-console.log(`Repository: ${process.env.GITHUB_REPOSITORY}`)
-console.log(`Event name: ${process.env.GITHUB_EVENT_NAME}`)
-console.log(`Event: ${process.env.GITHUB_EVENT}`)
+octokit.log.debug('Hello, World!')
+octokit.log.debug(`Repository: ${process.env.GITHUB_REPOSITORY}`)
+octokit.log.debug(`Event name: ${process.env.GITHUB_EVENT_NAME}`)
+octokit.log.debug(`Event: ${process.env.GITHUB_EVENT}`)
 
 const event = JSON.parse(process.env.GITHUB_EVENT || '{}')
 const workflowRunId = event.workflow_run.id
@@ -25,4 +25,21 @@ const pending = await octokit.request(
   },
 )
 
-console.log({ pending })
+octokit.log.debug(JSON.stringify(pending))
+
+const approval = await octokit.request(
+  'POST /repos/{owner}/{repo}/actions/runs/{run_id}/pending_deployments',
+  {
+    owner,
+    repo,
+    run_id: workflowRunId,
+    environment_ids: [161171787],
+    state: 'approved',
+    comment: 'Automatically approved by github action script.',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  },
+)
+
+octokit.log.debug(JSON.stringify(approval))
